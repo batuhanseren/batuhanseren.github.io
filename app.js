61b61a4ed4d0a4f3eb68913b4eea92ce57b46ef6
batuhanseren <76528441+batuhanseren@users.noreply.github.com>
2022-09-23 14:07:58 +0300

    first_change

--- app.js
+++ app.js
@@ -2,3 +2,35 @@ window.onload = function () {
   document.querySelector(".loader").style.display = "none";
 };
 
+let modal = document.querySelector(".modal");
+let body = document.querySelector("body");
+let modalClose = document.querySelector("#modal-close");
+let modalCh = document.querySelector(".modal-ch");
+let popup = document.querySelector("#popup");
+popup.addEventListener("click", function () {
+  modal.style.display = "flex";
+  body.style.overflow = "hidden";
+});
+modal.addEventListener(
+  "click",
+  function (event) {
+    if (
+      event.target.matches("#modal-close") ||
+      !event.target.closest(".modal-ch")
+    ) {
+      modal.style.display = "none";
+      body.style.overflow = "auto";
+    }
+  },
+  false
+);
+
+const share = document.getElementById("share");
+let link = document.querySelector("#sharelink");
+share.addEventListener("click", async (event) => {
+  const content = document.getElementById("copy-board").textContent;
+  await navigator.clipboard.writeText(content);
+  const copied = await navigator.clipboard.readText();
+  console.log(copied);
+  
+});
